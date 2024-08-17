96db307b37325be0d12687b38260fd1ffd73c9a6
Ragul-dravid <dhurai737@gmail.com>
2024-08-17 20:51:03 +0530

    .

--- src/config/URL.js
+++ src/config/URL.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://13.213.208.92:7080/ecssms/api/",
-  // baseURL: "https://hrisasia.com/ecssms/api/",
-  baseURL: "https://artylearning.com/artylearning/api/",
+  baseURL: "https://hrisasia.com/ecssms/api/",
+  // baseURL: "https://artylearning.com/artylearning/api/",
 });
 
 // Add a request interceptor
