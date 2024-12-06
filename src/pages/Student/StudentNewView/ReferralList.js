d3a83fcb09adb4767bddf9ff4e5f3b06eedce9f1
Rohini0410 <rohinidevi.cloudecs@gmail.com>
2024-12-06 11:14:29 +0530

    updated

--- src/pages/Student/StudentNewView/ReferralList.js
+++ src/pages/Student/StudentNewView/ReferralList.js
@@ -17,39 +17,20 @@ const ReferralList = () => {
     }, []);
 
     return (
-        <div className="container">
-            <div className="d-flex justify-content-end">
-                
-            </div>
+        <div className="container my-3">
             <table ref={tableRef} className="display">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Center</th>
-                        <th scope="col">Course</th>
-                        <th scope="col">Class Code</th>
-                        <th scope="col">Class Listing</th>
-                        <th scope="col">Teacher</th>
-                        <th scope="col">Start Time</th>
-                        <th scope="col">End Time</th>
-                        <th scope="col">Start Date</th>
-                        <th scope="col">End Date</th>
-                        <th scope="col">Duration in Class</th>
-                        <th scope="col">Attendance</th>
+                        <th scope="col">Center Name</th>
+                        <th scope="col">Referral Student Name</th>
+                        <th scope="col">New Student Name</th>
+                        <th scope="col">New Student Start Date</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">1</th>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
                         <td></td>
                         <td></td>
                         <td></td>
