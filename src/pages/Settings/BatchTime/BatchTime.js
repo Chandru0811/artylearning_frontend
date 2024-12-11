1d06624c7133638d52e39aa7e58f61c4b68ba87b
suriya94454 <suriyaecs22@gmail.com>
2024-12-11 14:23:13 +0530

    Add batch ui

---
+++ src/pages/Settings/BatchTime/BatchTime.js
@@ -0,0 +1,263 @@
+import React, { useEffect, useRef, useState } from "react";
+import "datatables.net-dt";
+import "datatables.net-responsive-dt";
+import $ from "jquery";
+import api from "../../../config/URL";
+import Delete from "../../../components/common/Delete";
+import { Link } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { MdViewColumn } from "react-icons/md";
+import BatchTimeEdit from "./BatchTimeEdit";
+
+const BatchTime = () => {
+  const tableRef = useRef(null);
+  // const storedScreens = JSON.parse(localStorage.getItem("screens") || "{}");
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [extraData, setExtraData] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     try {
+  //       const response = await api.get("/getAllSHGSetting");
+  //       setDatas(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+  //   getData();
+  // }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      const fetchedData = [
+        {
+          day: "Monday",
+          batchTime: "2.30 PM, 3.30 PM",
+        },
+        {
+          day: "Tuesday",
+          batchTime: "2.30 PM",
+        },
+        {
+          day: "Wednesday",
+          batchTime: "2.30 PM",
+        },
+        {
+          day: "Thursday",
+          batchTime: "2.30 PM",
+        },
+        {
+          day: "Friday",
+          batchTime: "2.30 PM",
+        },
+        {
+          day: "Saturday",
+          batchTime: "2.30 PM",
+        },
+        {
+          day: "Sunday",
+          batchTime: "2.30 PM",
+        },
+      ];
+      setDatas(fetchedData);
+      setLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      initializeDataTable();
+    }
+    return () => {
+      destroyDataTable();
+    };
+  }, [loading]);
+
+  const initializeDataTable = () => {
+    if ($.fn.DataTable.isDataTable(tableRef.current)) {
+      // DataTable already initialized, no need to initialize again
+      return;
+    }
+    $(tableRef.current).DataTable({
+      responsive: true,
+      columnDefs: [{ orderable: false, targets: -1 }],
+    });
+  };
+
+  const destroyDataTable = () => {
+    const table = $(tableRef.current).DataTable();
+    if (table && $.fn.DataTable.isDataTable(tableRef.current)) {
+      table.destroy();
+    }
+  };
+
+  const refreshData = async () => {
+    destroyDataTable();
+    setLoading(true);
+    try {
+      const response = await api.get("/getAllSHGSetting");
+      setDatas(response.data);
+      initializeDataTable(); // Reinitialize DataTable after successful data update
+    } catch (error) {
+      console.error("Error refreshing data:", error);
+    }
+    setLoading(false);
+  };
+  const handleDataShow = () => {
+    if (!loading) {
+      setExtraData(!extraData);
+      initializeDataTable();
+    }
+    return () => {
+      destroyDataTable();
+    };
+  };
+
+  const extractDate = (dateString) => {
+    if (!dateString) return ""; // Handle null or undefined date strings
+    return dateString.substring(0, 10); // Extracts the date part in "YYYY-MM-DD"
+  };
+
+  return (
+    <div className="container my-4">
+      <ol
+        className="breadcrumb my-3 px-1"
+        style={{ listStyle: "none", padding: 0, margin: 0 }}
+      >
+        <li>
+          <Link to="/" className="custom-breadcrumb">
+            Home
+          </Link>
+          <span className="breadcrumb-separator"> &gt; </span>
+        </li>
+        <li>
+          Settings
+          <span className="breadcrumb-separator"> &gt; </span>
+        </li>
+        <li className="breadcrumb-item active" aria-current="page">
+          Batch Time
+        </li>
+      </ol>
+      {/* {storedScreens?.levelCreate &&  */}
+
+      {loading ? (
+        <div className="loader-container">
+          <div className="loading">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      ) : (
+        <div className="table-responsive">
+          <table ref={tableRef} className="display">
+            <thead>
+              <tr>
+                <th scope="col" style={{ whiteSpace: "nowrap" }}>
+                  S No
+                </th>
+                <th scope="col" className="text-center">
+                  Day
+                </th>
+                <th scope="col" className="text-center">
+                  Batch Time
+                </th>
+                {extraData && (
+                  <th
+                    scope="col"
+                    class="sorting"
+                    tabindex="0"
+                    aria-controls="DataTables_Table_0"
+                    rowspan="1"
+                    colspan="1"
+                    aria-label="CreatedBy: activate to sort column ascending: activate to sort column ascending"
+                    style={{ width: "92px" }}
+                  >
+                    CreatedBy
+                  </th>
+                )}
+                {extraData && (
+                  <th
+                    scope="col"
+                    class="sorting"
+                    tabindex="0"
+                    aria-controls="DataTables_Table_0"
+                    rowspan="1"
+                    colspan="1"
+                    aria-label="CreatedAt: activate to sort column ascending: activate to sort column ascending"
+                    style={{ width: "92px" }}
+                  >
+                    CreatedAt
+                  </th>
+                )}
+                {extraData && (
+                  <th
+                    scope="col"
+                    class="sorting"
+                    tabindex="0"
+                    aria-controls="DataTables_Table_0"
+                    rowspan="1"
+                    colspan="1"
+                    aria-label="UpdatedBy: activate to sort column ascending: activate to sort column ascending"
+                    style={{ width: "92px" }}
+                  >
+                    UpdatedBy
+                  </th>
+                )}
+                {extraData && (
+                  <th
+                    scope="col"
+                    class="sorting"
+                    tabindex="0"
+                    aria-controls="DataTables_Table_0"
+                    rowspan="1"
+                    colspan="1"
+                    aria-label="UpdatedAt: activate to sort column ascending: activate to sort column ascending"
+                    style={{ width: "92px" }}
+                  >
+                    UpdatedAt
+                  </th>
+                )}
+                <th scope="col" className="text-center">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {datas.map((data, index) => (
+                <tr key={index}>
+                  <th scope="row">{index + 1}</th>
+                  <td className="text-center">{data.day}</td>
+                  <td className="text-center">{data.batchTime}</td>
+                  {extraData && <td>{data.createdBy}</td>}
+                  {extraData && <td>{extractDate(data.createdAt)}</td>}
+                  {extraData && <td>{data.updatedBy}</td>}
+                  {extraData && <td>{extractDate(data.updatedAt)}</td>}
+                  <td className="text-center">
+                    {/* {storedScreens?.levelRead && ( */}
+                    {/* <Link to={`/shg/view/${data.id}`}>
+                                        <button className="btn btn-sm">
+                                            <FaEye />
+                                        </button>
+                                    </Link> */}
+                    {/* )} */}
+                    {/* {storedScreens?.levelUpdate && ( */}
+                    <BatchTimeEdit id={data.id} onSuccess={refreshData} />
+                    {/* )} */}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default BatchTime;
---
+++ src/pages/Settings/BatchTime/BatchTimeEdit.js
@@ -0,0 +1,242 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { FaEdit } from "react-icons/fa";
+import * as Yup from "yup";
+import api from "../../../config/URL";
+import { toast } from "react-toastify";
+import { IoCloseCircleOutline } from "react-icons/io5";
+
+function BatchTimeEdit({ id, onSuccess }) {
+  const [show, setShow] = useState(false);
+  const [loadIndicator, setLoadIndicator] = useState(false);
+  const userName = localStorage.getItem("userName");
+  const [isModified, setIsModified] = useState(false);
+  const [paymentTerm, setPaymentTerm] = useState(null);
+  const [fields, setFields] = useState([
+    {
+      id: 1,
+      batchTime: "",
+    },
+  ]);
+
+  const validationSchema = Yup.object({
+    batchTime: Yup.string().required("*Batch Time is required"),
+  });
+
+  // const getData = async () => {
+  //   try {
+  //     const response = await api.get(`/getAllSHGSettingById/${id}`);
+  //     formik.setValues(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching data ", error);
+  //   }
+  // };
+
+  const getData = async () => {
+    try {
+      const hardcodedData = {
+        id: 1,
+        day: "Monday",
+        batchTime: "10:00",
+      };
+
+      formik.setValues(hardcodedData);
+    } catch (error) {
+      console.error("Error setting data ", error);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      day: "",
+      batchTime: "",
+      updatedBy: userName,
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      // console.log(values);
+      setLoadIndicator(true);
+      try {
+        const response = await api.put(`/updateSHGSetting/${id}`, values, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if (response.status === 200) {
+          onSuccess();
+          handleClose();
+          toast.success(response.data.message);
+        } else {
+          toast.error(response.data.message);
+        }
+      } catch (error) {
+        toast.error(error);
+      } finally {
+        setLoadIndicator(false);
+      }
+    },
+    enableReinitialize: true,
+    validateOnChange: true,
+    validateOnBlur: true,
+    validate: (values) => {
+      if (
+        Object.values(values).some(
+          (value) => typeof value === "string" && value.trim() !== ""
+        )
+      ) {
+        setIsModified(true);
+      } else {
+        setIsModified(false);
+      }
+    },
+  });
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => {
+    setShow(true);
+    setIsModified(false);
+    getData();
+  };
+
+  // useEffect(() => {
+  //   getData();
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  const addFields = () => {
+    setFields([
+      ...fields,
+      {
+        id: fields.length + 1,
+        batchTime: "",
+      },
+    ]);
+  };
+
+  const deleteFields = (id) => {
+    setFields(fields.filter((field) => field.id !== id));
+  };
+
+  return (
+    <>
+      <button className="btn btn-sm" onClick={handleShow}>
+        <FaEdit />
+      </button>
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        backdrop={isModified ? "static" : true}
+        keyboard={isModified ? false : true}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title className="headColor">Batch Time Edit</Modal.Title>
+        </Modal.Header>
+        <form
+          onSubmit={formik.handleSubmit}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !formik.isSubmitting) {
+              e.preventDefault(); // Prevent default form submission
+            }
+          }}
+        >
+          <Modal.Body>
+            <div className="container">
+              <div className="row py-4">
+                <div className="col-md-6 col-12 mt-3">
+                  <label className="form-label">
+                    Day<span className="text-danger">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    className={`form-control  ${
+                      formik.touched.day && formik.errors.day
+                        ? "is-invalid"
+                        : ""
+                    }`}
+                    {...formik.getFieldProps("day")}
+                  />
+                  {formik.touched.day && formik.errors.day && (
+                    <div className="invalid-feedback">{formik.errors.day}</div>
+                  )}
+                </div>
+
+                <div className="col-md-6 col-12 mb-2">
+                  {fields.map((row, index) => (
+                    <div key={row.id}>
+                      <div className="d-flex justify-content-between mt-3">
+                        <span>
+                          <label className="form-label">
+                            Batch Time<span className="text-danger">*</span>
+                          </label>
+                        </span>
+                        {row.id > 1 && (
+                          <span>
+                             <button
+                              type="button"
+                              className="btn btn-sm pb-2"
+                              onClick={() => deleteFields(row.id)}
+                            >
+                              <IoCloseCircleOutline style={{ color: "red", fontSize: "18px"  }} />
+                            </button>
+                          </span>
+                        )}
+                      </div>
+                      <input
+                        type="time"
+                        className={`form-control  ${
+                          formik.touched.batchTime && formik.errors.batchTime
+                            ? "is-invalid"
+                            : ""
+                        }`}
+                        {...formik.getFieldProps("batchTime")}
+                      />
+                      {formik.touched.batchTime && formik.errors.batchTime && (
+                        <div className="invalid-feedback">
+                          {formik.errors.batchTime}
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-danger mt-3"
+                    onClick={addFields}
+                  >
+                    Add more
+                  </button>
+                </div>
+              </div>
+            </div>
+            <Modal.Footer>
+              <Button type="button" variant="secondary" onClick={handleClose}>
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                className="btn btn-button btn-sm"
+                disabled={loadIndicator}
+                onClick={formik.handleSubmit}
+              >
+                {loadIndicator && (
+                  <span
+                    className="spinner-border spinner-border-sm me-2"
+                    aria-hidden="true"
+                  ></span>
+                )}
+                Update
+              </Button>
+            </Modal.Footer>
+          </Modal.Body>
+        </form>
+      </Modal>
+    </>
+  );
+}
+
+export default BatchTimeEdit;
