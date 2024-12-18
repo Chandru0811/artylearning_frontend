75bb56ec42fe7328825b16cf839000ae07246c7c
Chandru0811 <chandru08112000@gmail.com>
2024-12-18 20:44:09 +0530

    Dashboard

--- src/pages/NewDashboard.js
+++ src/pages/NewDashboard.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ApexCharts from "react-apexcharts";
+import api from "../config/URL";
+import { toast } from "react-toastify";
 
 function NewDashboard() {
-  const progressValue1 = 25;
-  const progressValue2 = 50;
-  const progressValue3 = 70;
-  const fontFamily = "'Outfit', system-ui, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'";
+  const [datas, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const response = await api.get("getOverAllSmsRevenueReport");
+      setData(response.data);
+    } catch (e) {
+      toast.error("Error Fetching Dashboard Data");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
+  const fontFamily =
+    "'Outfit', system-ui, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'";
 
   const lineChartOptions = {
     chart: {
@@ -35,20 +55,7 @@ function NewDashboard() {
       size: 0,
     },
     xaxis: {
-      categories: [
-        "01 Jan",
-        "02 Jan",
-        "03 Jan",
-        "04 Jan",
-        "05 Jan",
-        "06 Jan",
-        "07 Jan",
-        "08 Jan",
-        "09 Jan",
-        "10 Jan",
-        "11 Jan",
-        "12 Jan",
-      ],
+      categories: datas?.revenueOverTimeReport?.lineChartData?.categories,
       labels: {
         style: {
           fontFamily: fontFamily,
@@ -90,27 +97,14 @@ function NewDashboard() {
     colors: ["#ABBDD3", "#287F71", "#EB862A"],
   };
 
-  const lineChartSeries = [
-    {
-      name: "Total Enrolment",
-      data: [45, 52, 38, 24, 33, 26, 21],
-    },
-    {
-      name: "Total Package",
-      data: [35, 41, 62, 42, 13, 18, 29],
-    },
-    {
-      name: "Total Revenue",
-      data: [87, 57, 74, 99, 75, 38, 62],
-    },
-  ];
+  const lineChartSeries = datas?.revenueOverTimeReport?.lineChartData?.series;
 
   const gaugeChartOptions = {
     chart: {
       height: 280,
       type: "radialBar",
     },
-    series: [67],
+    series: [datas?.registeredUsersReport?.gaugeChartData?.value],
     colors: ["#287F71"],
     plotOptions: {
       radialBar: {
@@ -138,9 +132,10 @@ function NewDashboard() {
     },
     labels: ["Progress"],
   };
-  
-  const gaugeChartSeries = [67];
-  
+
+  const gaugeChartSeries = [
+    datas?.registeredUsersReport?.gaugeChartData?.value,
+  ];
 
   const lineChartOptions1 = {
     chart: {
@@ -165,7 +160,7 @@ function NewDashboard() {
       size: 0,
     },
     xaxis: {
-      categories: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], // Days of the week
+      categories: datas?.revenueGrowthByDay?.categories,
     },
     tooltip: {
       y: [
@@ -191,264 +186,188 @@ function NewDashboard() {
     },
     colors: ["#ABBDD3", "#287F71", "#EB862A"],
   };
-  
-  const lineChartSeries1 = [
-    {
-      name: "Total Enrolment",
-      data: [45, 52, 38, 24, 33, 26, 21], // Data for Monday to Sunday
-    },
-    {
-      name: "Total Package",
-      data: [35, 41, 62, 42, 13, 18, 29], // Data for Monday to Sunday
-    },
-    {
-      name: "Total Revenue",
-      data: [87, 57, 74, 99, 75, 38, 62], // Data for Monday to Sunday
-    },
-  ];
-  
-  
+
+  const lineChartSeries1 = datas?.revenueGrowthByDay?.series;
 
   return (
-    <div className="container mt-4">
-      <div className="row mt-3">
-        <div className="col-md-3 mb-3">
-          <div
-            className="card shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="card-body">
-              <h6 className="card-title text-secondary">Lead Count</h6>
-              <h5 className="card-text fw-bold text-dark">132</h5>
-              <span className="d-flex align-items-center justify-content-between">
-                <span
-                  className="text-success fw-bold me-2"
-                  style={{
-                    backgroundColor: "#e6f8eb",
-                    padding: "2px 5px",
-                    borderRadius: "5px",
-                    fontSize: "13px",
-                  }}
-                >
-                  ↑ 12.95%
-                </span>
-                <small className="text-secondary" style={{ fontSize: "10px" }}>
-                  Compared to last month
-                </small>
-              </span>
-            </div>
+    <>
+      {loading ? (
+        <div className="loader-container">
+          <div className="loading">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
         </div>
-        <div className="col-md-3 mb-3">
-          <div
-            className="card shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="card-body">
-              <h6 className="card-title text-secondary">Student</h6>
-              <h5 className="card-text fw-bold text-dark">103</h5>
-              <span className="d-flex align-items-center">
-                <span
-                  className="text-danger fw-bold me-2"
-                  style={{
-                    backgroundColor: "#fdeaea",
-                    padding: "2px 5px",
-                    borderRadius: "5px",
-                    fontSize: "13px",
-                  }}
-                >
-                  ↓ 0.33%
-                </span>
-                <small className="text-secondary" style={{ fontSize: "10px" }}>
-                  Compared to last month
-                </small>
-              </span>
-            </div>
+      ) : (
+        <div className="container mt-4">
+          <div className="row mt-3">
+            {datas.revenueGrowthByMonth &&
+              datas?.revenueGrowthByMonth.map((data, index) => (
+                <div className="col-md-3 mb-3">
+                  <div
+                    className="card shadow-sm border-0"
+                    style={{ borderRadius: "10px" }}
+                  >
+                    <div className="card-body">
+                      <h6 className="card-title text-secondary">
+                        {data.title}
+                      </h6>
+                      <h5 className="card-text fw-bold text-dark">
+                        {index === datas.revenueGrowthByMonth.length - 1
+                          ? `$ ${data.current}`
+                          : data.current}
+                      </h5>
+                      <span className="d-flex align-items-center justify-content-between">
+                        {data.percentageChange >= 0 ? (
+                          <span
+                            className="text-success fw-bold me-2"
+                            style={{
+                              backgroundColor: "#e6f8eb",
+                              padding: "2px 5px",
+                              borderRadius: "5px",
+                              fontSize: "13px",
+                            }}
+                          >
+                            ↑ {data.percentageChange}%
+                          </span>
+                        ) : (
+                          <span
+                            className="text-danger fw-bold me-2"
+                            style={{
+                              backgroundColor: "#fdeaea",
+                              padding: "2px 5px",
+                              borderRadius: "5px",
+                              fontSize: "13px",
+                            }}
+                          >
+                            ↓ {Math.abs(data.percentageChange)}%
+                          </span>
+                        )}
+                        <small
+                          className="text-secondary"
+                          style={{ fontSize: "10px" }}
+                        >
+                          {data.comparison}
+                        </small>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              ))}
           </div>
-        </div>
-        <div className="col-md-3 mb-3">
-          <div
-            className="card shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="card-body">
-              <h6 className="card-title text-secondary">Teacher</h6>
-              <h5 className="card-text fw-bold text-dark">54</h5>
-              <span className="d-flex align-items-center">
-                <span
-                  className="text-success fw-bold me-2"
-                  style={{
-                    backgroundColor: "#e6f8eb",
-                    padding: "2px 5px",
-                    borderRadius: "5px",
-                    fontSize: "13px",
-                  }}
-                >
-                  ↑ 12.95%
-                </span>
-                <small className="text-secondary" style={{ fontSize: "10px" }}>
-                  Compared to last month
-                </small>
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-3 mb-3">
-          <div
-            className="card shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="card-body">
-              <h6 className="card-title text-secondary">Total Revenue</h6>
-              <h5 className="card-text fw-bold text-dark">$10,499.93</h5>
-              <span className="d-flex align-items-center">
-                <span
-                  className="text-danger fw-bold me-2"
-                  style={{
-                    backgroundColor: "#fdeaea",
-                    padding: "2px 5px",
-                    borderRadius: "5px",
-                    fontSize: "13px",
-                  }}
-                >
-                  ↓ 0.33%
-                </span>
-                <small className="text-secondary" style={{ fontSize: "10px" }}>
-                  Compared to last month
-                </small>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-8 mb-4">
-          <div
-            className="card shadow-sm p-3 h-100 shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="d-flex justify-content-between">
-              <h6 className="card-title">Leads Trend</h6>
-              <i className="fas fa-ellipsis-h"></i> {/* Triple dot icon */}
-            </div>
-            <ApexCharts
-              options={lineChartOptions}
-              series={lineChartSeries}
-              type="line"
-              height={200}
-            />
-          </div>
-        </div>
-        <div className="col-md-4 mb-4">
-          <div
-            className="card shadow-sm p-3 h-100 shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="d-flex justify-content-between">
-              <h6 className="card-title">Capacity Status</h6>
-            </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-secondary m-1" style={{ fontSize: "14px" }}>
-                Arty Learning @ HG
-              </p>
-              <span style={{ fontSize: "13px" }} className="fw-bold">
-                {progressValue1}%
-              </span>
-            </div>
-            <div className="progress mb-3" style={{ height: "7px" }}>
-              {" "}
-              {/* Adjust height */}
+          <div className="row">
+            <div className="col-md-8 mb-4">
               <div
-                className="progress-bar"
-                role="progressbar"
-                style={{
-                  width: `${progressValue1}%`,
-                  backgroundColor: "#287F71",
-                }}
-                aria-valuenow="25"
-                aria-valuemin="0"
-                aria-valuemax="100"
-              ></div>
+                className="card shadow-sm p-3 h-100 shadow-sm border-0"
+                style={{ borderRadius: "10px" }}
+              >
+                <div className="d-flex justify-content-between">
+                  <h6 className="card-title">Leads Trend</h6>
+                  <i className="fas fa-ellipsis-h"></i> {/* Triple dot icon */}
+                </div>
+                {!loading && lineChartSeries && lineChartSeries.length > 0 && (
+                  <ApexCharts
+                    options={lineChartOptions}
+                    series={lineChartSeries}
+                    type="line"
+                    height={200}
+                  />
+                )}
+              </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-secondary m-1" style={{ fontSize: "14px" }}>
-                Arty Learning @ AMK
-              </p>
-              <span style={{ fontSize: "13px" }} className="fw-bold">
-                {progressValue2}%
-              </span>
-            </div>
-            <div className="progress mb-3" style={{ height: "7px" }}>
+            <div className="col-md-4 mb-4">
               <div
-                className="progress-bar"
-                role="progressbar"
+                className="card shadow-sm p-3 h-100 shadow-sm border-0"
                 style={{
-                  width: `${progressValue2}%`,
-                  backgroundColor: "#287F71",
+                  borderRadius: "10px",
+                  maxHeight: "270px",
+                  overflowY: "auto",
                 }}
-                aria-valuenow="50"
-                aria-valuemin="0"
-                aria-valuemax="100"
-              ></div>
+              >
+                <div className="d-flex justify-content-between">
+                  <h6 className="card-title">Capacity Status</h6>
+                </div>
+                {datas.productionSalesComparisonReport.progressValues &&
+                  datas?.productionSalesComparisonReport?.progressValues.map(
+                    (center, index) => (
+                      <div key={index}>
+                        <div className="d-flex justify-content-between">
+                          <p
+                            className="text-secondary m-1"
+                            style={{ fontSize: "14px" }}
+                          >
+                            {center.centerName}
+                          </p>
+                          <span
+                            style={{ fontSize: "13px" }}
+                            className="fw-bold"
+                          >
+                            {center.value}%
+                          </span>
+                        </div>
+                        <div
+                          className="progress mb-3"
+                          style={{ height: "7px" }}
+                        >
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${center.value}%`,
+                              backgroundColor:
+                                center.value === 0 ? "#f8d7da" : "#287F71", // Change color for 0% values
+                            }}
+                            aria-valuenow={center.value}
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                          ></div>
+                        </div>
+                      </div>
+                    )
+                  )}
+              </div>
             </div>
-            <div className="d-flex justify-content-between">
-              <p className="text-secondary m-1" style={{ fontSize: "14px" }}>
-                Arty Learning @ BB
-              </p>
-              <span style={{ fontSize: "13px" }} className="fw-bold">
-                {progressValue3}%
-              </span>
-            </div>
-            <div className="progress mb-3" style={{ height: "7px" }}>
+
+            <div className="col-md-8 col-12 mb-4">
               <div
-                className="progress-bar"
-                role="progressbar"
-                style={{
-                  width: `${progressValue3}%`,
-                  backgroundColor: "#287F71",
-                }}
-                aria-valuenow="70"
-                aria-valuemin="0"
-                aria-valuemax="100"
-              ></div>
+                className="card shadow-sm p-3 h-100 border-0"
+                style={{ borderRadius: "10px" }}
+              >
+                <div className="d-flex justify-content-between">
+                  <h6 className="card-title">Revenue Report</h6>
+                </div>
+                <ApexCharts
+                  options={lineChartOptions1}
+                  series={lineChartSeries1}
+                  type="line"
+                  height={200}
+                />
+              </div>
+            </div>
+            <div className="col-md-4 mb-4">
+              <div
+                className="card shadow-sm p-3 h-100 shadow-sm border-0"
+                style={{ borderRadius: "10px" }}
+              >
+                <div className="d-flex justify-content-between">
+                  <h6 className="card-title">Total Capacity</h6>
+                  <i className="fas fa-ellipsis-h"></i>
+                </div>
+                <ApexCharts
+                  options={gaugeChartOptions}
+                  series={gaugeChartSeries}
+                  type="radialBar"
+                  height={300}
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-md-8 col-12 mb-4">
-          <div
-            className="card shadow-sm p-3 h-100 border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="d-flex justify-content-between">
-              <h6 className="card-title">Revenue Report</h6>
-            </div>
-            <ApexCharts
-              options={lineChartOptions1}
-              series={lineChartSeries1}
-              type="line"
-              height={200}
-            />
-          </div>
-        </div>
-        <div className="col-md-4 mb-4">
-          <div
-            className="card shadow-sm p-3 h-100 shadow-sm border-0"
-            style={{ borderRadius: "10px" }}
-          >
-            <div className="d-flex justify-content-between">
-              <h6 className="card-title">Total Capacity</h6>
-              <i className="fas fa-ellipsis-h"></i>
-            </div>
-            <ApexCharts
-              options={gaugeChartOptions}
-              series={gaugeChartSeries}
-              type="radialBar"
-              height={300}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
