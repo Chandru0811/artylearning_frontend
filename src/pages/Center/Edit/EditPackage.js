5fb16cedb590024dc83bec7641768b2e89f3028b
prem-pk24 <premvp24@gmail.com>
2024-12-18 19:37:11 +0530

    bug fixed

--- src/pages/Center/Edit/EditPackage.js
+++ src/pages/Center/Edit/EditPackage.js
@@ -88,7 +88,11 @@ function EditPackage({ id, onSuccess }) {
   }, []);
   return (
     <>
-      <button type="button" className="btn" onClick={handleShow}>
+      <button type="button"
+        style={{
+          whiteSpace: "nowrap",
+        }}
+        className="btn btn-normal text-start" onClick={handleShow}>
         <FaEdit />
       </button>
 
